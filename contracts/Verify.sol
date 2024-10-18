79f2924d5e26c446b5bb27b33a19421d2fcfd089
Boguk Tatiana <boguk224@gmail.com>
2024-10-19 02:27:44 +0300

    Initial commit: Smart contract skeleton

--- contracts/Verify.sol
+++ contracts/Verify.sol
@@ -16,9 +16,9 @@ contract CertificateVerify is ERC721, Ownable {
     }
 
     mapping(uint256 => Certificate) private certificates; 
-    uint256 private nextTokenId; // Счетчик для уникальных идентификаторов токенов
+    uint256 private nextTokenId; 
 
-    event CertificateIssued(uint256 indexed tokenId, addrQess indexed issuer, string data, string pdfLink);
+    event CertificateIssued(uint256 indexed tokenId, address indexed issuer, string data, string pdfLink);
     event CertificateVerified(uint256 indexed tokenId);
     event CertificateRejected(uint256 indexed tokenId, string reason);
     
@@ -27,16 +27,14 @@ contract CertificateVerify is ERC721, Ownable {
         nextTokenId = 1; 
     }
 
-    // Функция для загрузки сертификата пользователем в лк
     function issueCertificate(address to, address reviewer, string memory data, string memory pdfLink) external onlyOwner {
-        uint256 tokenId = nextTokenId; // Уникальный идентификатор для сертификата
+        uint256 tokenId = nextTokenId; 
         certificates[tokenId] = Certificate(tokenId, data, msg.sender, reviewer, false, pdfLink, true);
         nextTokenId++; 
 
         emit CertificateIssued(tokenId, msg.sender, data, pdfLink);
     }
 
-    // Функция для подтверждения сертификатов
     function verifyAndMintToken(uint256 tokenId) external {
         require(msg.sender == certificates[tokenId].reviewer, "Not authorized to verify");
         require(certificates[tokenId].isPending, "Certificate not pending");
@@ -44,27 +42,23 @@ contract CertificateVerify is ERC721, Ownable {
         certificates[tokenId].isPending = false; 
         certificates[tokenId].isVerified = true; 
 
-        uint256 newTokenId = nextTokenId;       //Создание нфт токена
+        uint256 newTokenId = nextTokenId;       
         _mint(certificates[tokenId].issuer, newTokenId); 
 
         emit CertificateVerified(newTokenId);
         nextTokenId++; 
     }
 
-    // Функция для отклонения сертификата
     function rejectionToken(uint256 tokenId) external {
         require(msg.sender == certificates[tokenId].reviewer, "Not authorized to reject");
         require(certificates[tokenId].isPending, "Certificate not pending");
 
-        // Логика отклонения сертификата
         certificates[tokenId].isPending = false; 
         certificates[tokenId].isVerified = false; 
 
-        // Эмитируем событие с указанием причины
         emit CertificateRejected(tokenId, "Certificate has been rejected");
     }
 
-    // Функция для получения информации о сертификате
     function getCertificate(uint256 tokenId) external view returns (Certificate memory) {
         return certificates[tokenId];
     }
